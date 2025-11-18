cb11045eec053199c73e8f4522ec831eb976d2ba
Edgar Q <edgar.quezada.com>
2025-11-18 23:07:31 +0100

    update babel.config.js, tailwind.config.js and nativewind-env.d.ts files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { StatusBar, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className={"text-blue-500"}> Uber Clone</Text>
+      <Text className={"text-blue-500 text-bold"}> Uber Clone</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
